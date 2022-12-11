d9df3e5a10bd42c468699d97efe320320cb339cc
yzeek <isaac.cohen1978@gmail.com>
2022-12-11 13:02:31 +0200

    chaned cookie ssl check

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -6,7 +6,7 @@ const buildClient = ({ req }) => {
 
     return axios.create({
       baseURL:
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+        "http://microservices.ic-digitalmarketing.com",
       headers: req.headers,
     });
   } else {
