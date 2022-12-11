93dbfce34a2696a8a72d03569f7f1dabeb5b0865
yzeek <isaac.cohen1978@gmail.com>
2022-12-11 14:32:13 +0200

    ingress

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const buildClient = ({ req }) => {
   if (typeof window === "undefined") {
     // We are on the server
-
     return axios.create({
       baseURL:
         "http://microservices.ic-digitalmarketing.com",
