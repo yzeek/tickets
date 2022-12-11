089bb8e1c7018679359caa5a2a396d174fdea372
yzeek <isaac.cohen1978@gmail.com>
2022-12-11 18:13:17 +0200

    checkout latest

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const buildClient = ({ req }) => {
   if (typeof window === "undefined") {
-    // We are on the server
+    // We are on the  server
 
     return axios.create({
       baseURL:
